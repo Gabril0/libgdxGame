312972ede3d980d9effad6a62a90e177ad99e0df
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-05-10 00:52:39 -0300

    Add files via upload

---
+++ BulletHell/core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("PlayerBaseSprite.png");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ BulletHell/core/src/com/mygdx/game/Player.java
@@ -0,0 +1,8 @@
+package com.mygdx.game;
+
+public class Player {
+    public void createPlayer() {}
+    public void renderPlayer() {}
+    public void disposePlayer() {}
+
+}
