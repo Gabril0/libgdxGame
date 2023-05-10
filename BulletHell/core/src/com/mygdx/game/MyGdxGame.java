8be514975afe2e7d67729cff294d140723676360
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-05-10 08:34:28 -0300

    Add files via upload

--- BulletHell/core/src/com/mygdx/game/MyGdxGame.java
+++ BulletHell/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,24 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	Player player = new Player();
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("PlayerBaseSprite.png");
+		player.createPlayer();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		player.renderPlayer();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		player.disposePlayer();
 	}
 }
--- BulletHell/core/src/com/mygdx/game/Player.java
+++ BulletHell/core/src/com/mygdx/game/Player.java
@@ -1,8 +1,70 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Input;
+
 public class Player {
-    public void createPlayer() {}
-    public void renderPlayer() {}
-    public void disposePlayer() {}
+    private SpriteBatch batch; 
+	private Texture img;
+
+    private float spriteSizeX = 64, spriteSizeY = 64; //float for the size
+    private float spritePositionX = 0f, spritePositionY = 0f; //float for the position
+    private float speedX = 300f, speedY = 300f; //float for the speed
+
+    public void createPlayer() { //do these actions once the game starts
+        batch = new SpriteBatch();
+		img = new Texture("PlayerBaseSprite.png");
+    }
+    public void renderPlayer() { //do these actions every frame
+        movePlayer();
+        checkBounds();
+        drawPlayer();
+    }
+    public void disposePlayer() { //dispose of the player resources onde the game is closed
+        batch.dispose();
+		img.dispose();
+    }
+
+    private void movePlayer(){
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        spritePositionX += deltaTime * speedX * horizontalMovement();
+        spritePositionY += deltaTime * speedY * verticalMovement();
+    }
+
+    private void drawPlayer(){ //Draws the player sprite each frame
+        batch.begin();
+		batch.draw(img, spritePositionX, spritePositionY, spriteSizeX, spriteSizeY);
+		batch.end();
+    }
+
+    private void checkBounds(){ //checks if the player still on bounds
+        if (spritePositionX < 0) spritePositionX = 0;
+        if (spritePositionY < 0) spritePositionY = 0;
+        if (spritePositionX > 1300) spritePositionX = 1300;
+        if (spritePositionY > 702) spritePositionY = 702;
+
+    }
+
+    private int horizontalMovement(){
+        boolean left = Gdx.input.isKeyPressed(Input.Keys.A);  //gets the input for left movement
+        boolean right = Gdx.input.isKeyPressed(Input.Keys.D); //gets the input for right movement
+        //atributions
+        if(left && !right) return -1;
+        if(!left && right) return 1;
+        return 0;
+
+    }
+
+    private int verticalMovement(){
+        boolean up = Gdx.input.isKeyPressed(Input.Keys.W);  //gets the input for up movement
+        boolean down = Gdx.input.isKeyPressed(Input.Keys.S); //gets the input for down movement
+        //atributions
+        if(up && !down) return 1;
+        if(!up && down) return -1;
+        return 0;
+
+    }
 
 }
