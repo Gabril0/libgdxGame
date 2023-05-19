7615eed69d28f02c9b0e1ab02ad80dcf3a520249
Gabril <62163345+Gabril0@users.noreply.github.com>
2023-05-19 17:58:20 -0300

    Add files via upload

--- BulletHell/core/src/com/mygdx/game/MyGdxGame.java
+++ BulletHell/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.enemy.Enemy;
 
@@ -9,18 +10,23 @@ public class MyGdxGame extends ApplicationAdapter {
 	BulletPool bulletPool = new BulletPool(50);
 	CustomCursor cursor = new CustomCursor();
 	Enemy e;
+	Background bg = new Background();
+
 	
 	@Override
 	public void create () {
+		bg.createBackground(Gdx.graphics.getWidth()*1.96f, Gdx.graphics.getHeight(), "map1Slow.png", "map1Fast.png");
 		cursor.create();
 		e = new Enemy();
 		bulletPool.createBulletPool("PlayerBullet.png");
 		player.createPlayer();
+		
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1);
+		bg.renderBackground();
 		player.renderPlayer(bulletPool.getIsShooting());
 		bulletPool.renderBulletPool(player.getSpritePositionX(), player.getSpritePositionY(), 
 		player.getSpriteSizeX(), player.getSpriteSizeY(), player.rotateToCursor() - 90); //-op because bullets are in a diferent orientation
@@ -29,6 +35,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
+		bg.disposeBackground();
 		cursor.dispose();
 		player.disposePlayer();
 		bulletPool.disposeBulletPool();
