702a7a479337f1118b4f375948db562d44b51b1d
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2023-06-22 12:08:44 -0300

    Mirror is not shooting when it needs

---
+++ BulletHell/core/src/com/mygdx/game/enemy/Mirror.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.enemy;
+
+import com.badlogic.gdx.Gdx;
+
+public class Mirror extends Enemy {
+    State state;
+    private float pastHealth;
+
+    public Mirror(float positionX, float positionY, float speedX, float speedY, float health, String bulletImg, 
+    String bulletType, String sprite){
+        super(positionX, positionY, speedX, speedY, health, bulletImg, bulletType, sprite);
+
+        float width = Gdx.graphics.getWidth();
+        sizeX = width / 10;
+        sizeY = width / 10;
+
+        pastHealth = health;
+
+        state = new ChargeShootState(this);
+    }
+
+    @Override
+    protected void shot() {
+        // the delta time inside the BulletPool.render inhibits the one time shot
+        if(pastHealth > health){
+            state.shoot();
+            System.out.println("Shooting");
+            pastHealth = health;
+        }
+    }
+}
